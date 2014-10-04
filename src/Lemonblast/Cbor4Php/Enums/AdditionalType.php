854e88d9c35f40c8cb18e6155712e4e58630286c
lrezek <lukas@miratronix.com>
2014-10-04 18:33:20 -0400

    Renamed float types with underscores for consistency

--- src/Lemonblast/Cbor4Php/Enums/AdditionalType.php
+++ src/Lemonblast/Cbor4Php/Enums/AdditionalType.php
@@ -11,20 +11,23 @@ class AdditionalType {
     const BIT_MASK = 0b00011111;
     const MAX_VALUE = 23;
 
+    // Simple types
     const SIMPLE_FALSE = 20;
     const SIMPLE_TRUE = 21;
     const SIMPLE_NULL = 22;
     const SIMPLE_UNDEFINED = 23;
 
-    // Various sizes of uint
+    // Integer types
     const UINT_8 = 24;
     const UINT_16 = 25;
     const UINT_32 = 26;
     const UINT_64 = 27;
 
-    const FLOAT16 = 25;
-    const FLOAT32 = 26;
-    const FLOAT64 = 27;
+    // Float types
+    const FLOAT_16 = 25;
+    const FLOAT_32 = 26;
+    const FLOAT_64 = 27;
+
     const BREAK_TYPE = 31; // Break is a keyword in php, _TYPE appended
 }
 
