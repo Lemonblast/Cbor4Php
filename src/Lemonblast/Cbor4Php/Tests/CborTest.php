b1d5b860e7d7b544eda912dc6d45505f046b2a47
lrezek <lukas@miratronix.com>
2014-10-05 17:20:02 -0400

    Changed 'http://www.fake.com' curl_init to 'localhost' to avoid network traffic

--- src/Lemonblast/Cbor4Php/Tests/CborTest.php
+++ src/Lemonblast/Cbor4Php/Tests/CborTest.php
@@ -85,7 +85,7 @@ class CborTest extends \PHPUnit_Framework_TestCase
     function testEncodeUnknown()
     {
         // Open and close a resource, only way to get an unknown type in PHP.
-        $f = curl_init('http://www.fake.com');
+        $f = curl_init('localhost');
         curl_close($f);
 
         $encoded = Cbor::encode($f);
