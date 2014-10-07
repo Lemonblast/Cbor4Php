5439f4f10a34a4441a1c781347600092145e72ed
aweebs <aweebs@users.noreply.github.com>
2014-10-06 23:53:18 -0400

    Added basic unitTests for decodeString

--- src/Lemonblast/Cbor4Php/Tests/CborTest.php
+++ src/Lemonblast/Cbor4Php/Tests/CborTest.php
@@ -156,6 +156,27 @@ class CborTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals(pack('CCCCCCCCCCCCCCCCCCCCCCCCCCCC', 0b01111000, 0b11010, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90), $encoded);
     }
 
+    function testStringDecodeCharacter()
+    {
+        $decoded = Cbor::decode(pack('CC', 0b1100001, 97));
+
+        $this->assertEquals("a", $decoded);
+    }
+
+    function testStringDecodeLowerAlphabet()
+    {
+        $decoded = Cbor::decode(pack('CCCCCCCCCCCCCCCCCCCCCCCCCCCC', 0b01111000, 0b11010, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122));
+
+        $this->assertEquals("abcdefghijklmnopqrstuvwxyz", $decoded);
+    }
+
+    function testStringDecodeUpperAlphabet()
+    {
+        $decoded = Cbor::decode(pack('CCCCCCCCCCCCCCCCCCCCCCCCCCCC', 0b01111000, 0b11010, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90));
+
+        $this->assertEquals("ABCDEFGHIJKLMNOPQRSTUVWXYZ", $decoded);
+    }
+
     function testEncodeIntSequence()
     {
         $encoded = Cbor::encode(array(1,2,3));
