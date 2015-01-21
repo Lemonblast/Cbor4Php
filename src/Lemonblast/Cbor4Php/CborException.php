78bf0734f1c1760b7a73cd194fa3de9c01137b86
Lukas Rezek <lukas@miratronix.com>
2015-01-20 20:54:10 -0500

    Added PHPDoc to CborException

--- src/Lemonblast/Cbor4Php/CborException.php
+++ src/Lemonblast/Cbor4Php/CborException.php
@@ -1,5 +1,11 @@
 <?php namespace Lemonblast\Cbor4Php;
 
+/**
+ * Defines a CBOR Exception.
+ *
+ * Class CborException
+ * @package Lemonblast\Cbor4Php
+ */
 class CborException extends \Exception
 {
 }
